4148542941609cb3a17b5a47f8a13b2a58c09f12
Yann Martins <102706324+YannMartins@users.noreply.github.com>
2023-03-31 09:36:26 -0300

    Add files via upload

--- java-stickers/src/App.java
+++ java-stickers/src/App.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -12,6 +16,7 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 10 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -22,10 +27,34 @@ public class App {
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
+
         // exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001b[1mTítulo:\u001b[m " + filme.get("title"));
-            System.out.println("\u001b[1mPoster:\u001b[m " + filme.get("image"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+
+            String textoFigurinha;
+            InputStream imagemYann; 
+            if (classificacao >= 8.0) {
+                textoFigurinha = "LOUCURA";
+                imagemYann = new FileInputStream(new File("sobreposicao/Eu.jpeg"));
+            } else {
+                textoFigurinha = "MARROMENO";
+                imagemYann = new FileInputStream(new File("sobreposicao/file.jpeg"));
+            }
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemYann);
+
+            System.out.println("\u001b[1mTítulo:\u001b[m " + titulo);
+            System.out.println("\u001b[1mPoster:\u001b[m " + urlImagem);
             System.out.println("\u001b[1mNota:\u001b[m " + filme.get("imDbRating"));
             System.out.println();
         }
